738d0d3d659ee69cb12e5137d317bd5481d9f293
Leonardo Braz <lhleonardo@hotmail.com>
2015-09-03 15:58:22 -0400

    Modificando o arquivo de configuração e persistencia

--- src/main/java/br/com/estatistica/modelo/cadastro/PerfilAcesso.java
+++ src/main/java/br/com/estatistica/modelo/cadastro/PerfilAcesso.java
@@ -1,8 +1,16 @@
 package br.com.estatistica.modelo.cadastro;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class PerfilAcesso {
 
-	private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String nome;
 	private String descricao;
 
--- src/test/java/br/com/estatistica/hibernate/TestHibernate.java
+++ src/test/java/br/com/estatistica/hibernate/TestHibernate.java
@@ -7,23 +7,24 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import br.com.estatistica.modelo.cadastro.Estado;
+import br.com.estatistica.modelo.cadastro.PerfilAcesso;
 
 public class TestHibernate {
 
 	public static void main(String[] args) {
-		
-		Map<String,String> properties = new HashMap<String, String>();
+
+		PerfilAcesso acesso = new PerfilAcesso("Administradores", "Perfil para os administradores do sistema");
+
+		Map<String, String> properties = new HashMap<String, String>();
 		properties.put("javax.persistence.jdbc.user", "root");
 		properties.put("javax.persistence.jdbc.password", "delfos#123");
-		Estado estado = new Estado("Rondônia", "RO");
-		
+
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("delfos", properties);
-	    EntityManager manager = factory.createEntityManager();
-	    
-		manager.persist(estado);
-		
+		EntityManager manager = factory.createEntityManager();
+
+		manager.persist(acesso);
+
 		manager.close();
-    }
+	}
 
 }
