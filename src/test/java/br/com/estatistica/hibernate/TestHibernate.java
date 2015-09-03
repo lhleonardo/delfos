4a8312b574941d56892ad1bbfa355a8067b3d364
Leonardo Braz <lhleonardo@hotmail.com>
2015-09-03 09:13:53 -0400

    Criando as configurações de conexão

--- src/test/java/br/com/estatistica/hibernate/TestHibernate.java
+++ src/test/java/br/com/estatistica/hibernate/TestHibernate.java
@@ -1,5 +1,22 @@
 package br.com.estatistica.hibernate;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.estatistica.modelo.cadastro.Estado;
+
 public class TestHibernate {
+
+	public static void main(String[] args) {
+		Estado estado = new Estado("Rondônia", "RO");
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("delfos");
+	    EntityManager manager = factory.createEntityManager();
+	    
+		manager.persist(estado);
+		
+		manager.close();
+    }
 
 }
