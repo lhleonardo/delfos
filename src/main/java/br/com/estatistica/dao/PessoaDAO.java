c99d10f0d507a5367ed86e6bc741d3814fcd6e16
Leonardo Braz <lhleonardo@hotmail.com>
2015-09-03 00:03:17 -0400

    Atleração nas classes

---
+++ src/main/java/br/com/estatistica/dao/PessoaDAO.java
@@ -0,0 +1,63 @@
+package br.com.estatistica.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.estatistica.modelo.cadastro.Pessoa;
+
+public class PessoaDAO implements DAO<Pessoa> {
+
+	private Connection con;
+
+	public PessoaDAO(Connection con) {
+		this.con = con;
+	}
+
+	@Override
+	public void save(Pessoa modelo) throws SQLException, NullPointerException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(Pessoa modelo) throws SQLException, NullPointerException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Pessoa> getAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pessoa> getByNome(String nome) throws SQLException, NullPointerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pessoa getById(Integer id) throws SQLException, NullPointerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+		if (this.con.isClosed()) {
+			this.con.close();
+			this.con = null;
+			System.gc();
+		}
+	}
+}
