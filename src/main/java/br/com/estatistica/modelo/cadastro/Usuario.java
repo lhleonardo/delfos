821f0e716b9301835e910298734056f2af16e438
Leonardo Braz <lhleonardo@hotmail.com>
2015-09-02 23:28:48 -0400

    Criando modelos de perfis de acesso e usuários

--- src/main/java/br/com/estatistica/modelo/cadastro/Usuario.java
+++ src/main/java/br/com/estatistica/modelo/cadastro/Usuario.java
@@ -6,5 +6,60 @@ public class Usuario {
 	private String login;
 	private String descricao;
 	private PerfilAcesso perfilAcesso;
+	private Pessoa pessoa;
+
+	public Usuario(int id, String login, String descricao, PerfilAcesso perfilAcesso, Pessoa pessoa) {
+		this.id = id;
+		this.login = login;
+		this.descricao = descricao;
+		this.perfilAcesso = perfilAcesso;
+		this.pessoa = pessoa;
+	}
+
+	public Usuario(String login, PerfilAcesso perfilAcesso, Pessoa pessoa) {
+		this.login = login;
+		this.perfilAcesso = perfilAcesso;
+		this.pessoa = pessoa;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public PerfilAcesso getPerfilAcesso() {
+		return perfilAcesso;
+	}
+
+	public void setPerfilAcesso(PerfilAcesso perfilAcesso) {
+		this.perfilAcesso = perfilAcesso;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
 
 }
