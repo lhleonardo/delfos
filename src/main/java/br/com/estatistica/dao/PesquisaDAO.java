fe121b579a0b82e924fec090a3adad18e3533437
MarcoAnotnio13 <marcoantonio.tecinf@gmail.com>
2015-09-11 14:20:37 -0400

    Alterando Classe PesquisaDAO

--- src/main/java/br/com/estatistica/dao/PesquisaDAO.java
+++ src/main/java/br/com/estatistica/dao/PesquisaDAO.java
@@ -7,6 +7,15 @@ import java.util.List;
 import br.com.estatistica.modelos.Pesquisa;
 
 public class PesquisaDAO extends GenericDAO<Pesquisa> {
+	
+	private static final String SQL_SELECT = "SELECT * FROM Pesquisa";
+	private static final String SQL_SELECT_WHERE = SQL_SELECT + " WHERE login = ? AND senha = ?";
+	private static final String SQL_SELECT_BY_ID = SQL_SELECT + " WHERE id_usuario = ?";
+	private static final String SQL_SELECT_BY_LOGIN = SQL_SELECT + " WHERE login = ?";
+	private static final String SQL_SELECT_BY_LOGIN_AND_SENHA = SQL_SELECT_BY_LOGIN + " AND senha = ?";
+	private static final String SQL_INSERT = "INSERT INTO Usuario(login,senha,descricao,id_perfil_usuario) VALUES(?,?,?,?)";
+	private static final String SQL_UPDATE = "UPDATE Usuario SET login = ?, senha = ?, descricao = ?, id_perfil_usuario = ? WHERE id_usuario =?";
+	private static final String SQL_DELETE = "DELETE FROM Usuario WHERE id_usuario = ?";
 
 	public PesquisaDAO(Connection connection) {
 		super(connection);
