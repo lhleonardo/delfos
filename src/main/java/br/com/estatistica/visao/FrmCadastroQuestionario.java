07b0cc3c55a0254db63d69cf64b2aa94a662cf70
victorlukinha <victorlukinha@gmail.com>
2015-09-11 17:47:52 -0300

    Herdando design padrão

--- src/main/java/br/com/estatistica/visao/FrmCadastroQuestionario.java
+++ src/main/java/br/com/estatistica/visao/FrmCadastroQuestionario.java
@@ -1,13 +1,19 @@
 package br.com.estatistica.visao;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
-public class FrmCadastroQuestionario extends JFrame {
+public class FrmCadastroQuestionario extends GenericFormCadastro  {
 
 	private JPanel contentPane;
 
@@ -31,12 +37,35 @@ public class FrmCadastroQuestionario extends JFrame {
 	 * Create the frame.
 	 */
 	public FrmCadastroQuestionario() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		super();
+		setTitle("Cadastro de Pesquisas");
+
+		setResizable(false);
+
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.SOUTH);
+		panel.setLayout(null);
+
+		JLabel lblId = new JLabel("ID");
+		lblId.setBounds(11, 11, 17, 14);
+		panel.add(lblId);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		btnSalvar.setBounds(10, 428, 89, 23);
+		panel.add(btnSalvar);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(109, 428, 89, 23);
+		panel.add(btnCancelar);
+		
+		JLabel lblDescrio = new JLabel("Descrição");
+		lblDescrio.setBounds(11, 58, 64, 14);
+		panel.add(lblDescrio);
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		editorPane.setBounds(10, 84, 282, 75);
+		panel.add(editorPane);
 	}
 
 }
