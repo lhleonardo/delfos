298ad1939ad048943522c16c5d049e38da83b50f
Leonardo Braz <lhleonardo@hotmail.com>
2015-09-14 14:11:24 -0400

    criando teste de pesquisa para bairros.

--- src/test/java/br/com/estatistica/dao/BairroTest.java
+++ src/test/java/br/com/estatistica/dao/BairroTest.java
@@ -1,6 +1,7 @@
 package br.com.estatistica.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import br.com.estatistica.modelos.Bairro;
 import br.com.estatistica.util.ConnectionFactory;
@@ -9,13 +10,19 @@ public class BairroTest {
 
 	public static void main(String[] args) {
 		try (BairroDAO bDao = new BairroDAO(new ConnectionFactory().getConnection())) {
-			Bairro bairro = new Bairro();
-			bairro.setNome("Cafézinho");
-			bairro.setDescricao("Bairro de teste");
-			System.out.println(bDao.save(bairro));
+
+			pesquisar(bDao);
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 	}
+
+	private static void pesquisar(BairroDAO bDao) throws SQLException {
+	    List<Bairro> bairros = bDao.getAll();
+
+	    for (Bairro bairro : bairros) {
+	    	System.out.println(bairro.getId() + "\n" + bairro.getNome() + "\n" + bairro.getDescricao());
+	    }
+    }
 
 }
