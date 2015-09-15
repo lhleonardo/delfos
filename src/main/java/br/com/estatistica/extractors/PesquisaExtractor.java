c088bfe8d432f8f1e847ff707342de43922efc6a
MarcoAnotnio13 <marcoantonio.tecinf@gmail.com>
2015-09-14 22:19:35 -0400

    Criando Classe com um Método de Conversão de Data SQL para Util, e trabalhando nas Classes PesquisaDAO e PesquisaExtractor.

---
+++ src/main/java/br/com/estatistica/extractors/PesquisaExtractor.java
@@ -0,0 +1,23 @@
+package br.com.estatistica.extractors;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import br.com.estatistica.modelos.Pesquisa;
+
+public class PesquisaExtractor extends Extractable<Pesquisa> {
+
+	@Override
+	protected Pesquisa extractModel(ResultSet rs, Connection con)
+			throws SQLException {
+		Pesquisa pesq = new Pesquisa();
+		pesq.setId(rs.getInt("id_pesquisa"));
+		pesq.setDescricao(rs.getString("descricao"));
+		pesq.setLimiteDeEspecialistas(rs.getInt("limite_de_especialistas"));
+
+		return pesq;
+	}
+	
+	
+
+}
