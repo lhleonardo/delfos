4cbca3afdafec27d8737e6277d531a378b5d29ce
Leonardo Braz <lhleonardo@hotmail.com>
2015-09-23 16:55:58 -0400

    Criação do form de cadastro para bairros

--- src/main/java/br/com/estatistica/visao/FrmCadastroBairro.java
+++ src/main/java/br/com/estatistica/visao/FrmCadastroBairro.java
@@ -1,28 +1,106 @@
 package br.com.estatistica.visao;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.sql.Connection;
 
-import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class FrmCadastroBairro extends GenericFormCadastro {
-
+	
 	private static final long serialVersionUID = 1L;
-
+	private JPanel panel;
+	private JLabel lblCdigo;
+	private JTextField txtCodigo;
+	private JButton btnPesquisar;
+	private JLabel lblNome;
+	private JTextField textField;
+	private JLabel lblDescrio;
+	private JTextArea textArea;
+	private JPanel panel_1;
+	private JButton btnNovo;
+	private JButton btnSalvar;
+	private JButton btnExcluir;
+	private JButton btnCancelar;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
 			try {
-				FrmCadastroBairro frame = new FrmCadastroBairro();
+				FrmCadastroBairro frame = new FrmCadastroBairro(null);
 				frame.setVisible(true);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		});
 	}
-
-	public FrmCadastroBairro() {
-		this.setBounds(100, 100, 450, 300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+	
+	public FrmCadastroBairro(Connection connection) {
+		// TODO Refazer a tela.
+		super("Cadastro de Bairros");
+		this.initComponents();
 	}
 
+	private void initComponents() {
+		
+		this.panel = new JPanel();
+		this.getContentPane().add(this.panel, BorderLayout.CENTER);
+		this.panel.setLayout(null);
+		
+		this.lblCdigo = new JLabel("Código");
+		this.lblCdigo.setBounds(10, 11, 46, 14);
+		this.panel.add(this.lblCdigo);
+		
+		this.txtCodigo = new JTextField();
+		this.txtCodigo.setEditable(false);
+		this.txtCodigo.setBounds(10, 25, 46, 20);
+		this.panel.add(this.txtCodigo);
+		this.txtCodigo.setColumns(10);
+		
+		this.btnPesquisar = new JButton("");
+		this.btnPesquisar.setBounds(58, 24, 33, 23);
+		this.panel.add(this.btnPesquisar);
+		
+		this.lblNome = new JLabel("Nome");
+		this.lblNome.setBounds(101, 11, 46, 14);
+		this.panel.add(this.lblNome);
+		
+		this.textField = new JTextField();
+		this.textField.setBounds(101, 25, 363, 20);
+		this.panel.add(this.textField);
+		this.textField.setColumns(10);
+		
+		this.lblDescrio = new JLabel("Descrição");
+		this.lblDescrio.setBounds(10, 51, 46, 14);
+		this.panel.add(this.lblDescrio);
+		
+		this.textArea = new JTextArea();
+		this.textArea.setLineWrap(true);
+		this.textArea.setBounds(10, 65, 454, 219);
+		this.panel.add(this.textArea);
+		
+		this.panel_1 = new JPanel();
+		this.panel_1.setBounds(10, 295, 454, 43);
+		this.panel.add(this.panel_1);
+		this.panel_1.setLayout(null);
+
+		this.btnNovo = new JButton("Novo");
+		this.btnNovo.setBounds(0, 0, 92, 43);
+		this.panel_1.add(this.btnNovo);
+
+		this.btnSalvar = new JButton("Salvar");
+		this.btnSalvar.setBounds(91, 0, 92, 43);
+		this.panel_1.add(this.btnSalvar);
+
+		this.btnExcluir = new JButton("Excluir");
+		this.btnExcluir.setBounds(183, 0, 92, 43);
+		this.panel_1.add(this.btnExcluir);
+
+		this.btnCancelar = new JButton("Cancelar");
+		this.btnCancelar.setBounds(362, 0, 92, 43);
+		this.panel_1.add(this.btnCancelar);
+	}
 }
--- src/main/java/br/com/estatistica/visao/GenericFormCadastro.java
+++ src/main/java/br/com/estatistica/visao/GenericFormCadastro.java
@@ -15,83 +15,81 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class GenericFormCadastro extends JFrame {
-
+	
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JPanel panel_2;
-
+	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GenericFormCadastro frame = new GenericFormCadastro();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		EventQueue.invokeLater(() -> {
+			try {
+				GenericFormCadastro frame = new GenericFormCadastro();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		});
 	}
-
+	
 	/**
 	 * Create the frame.
 	 */
 	public GenericFormCadastro(String nameFrame) {
-		initComponents(nameFrame);
+		this.initComponents(nameFrame);
 	}
-
+	
 	public GenericFormCadastro() {
-		setResizable(false);
-		initComponents("Default crud");
+		this.setResizable(false);
+		this.initComponents("Default crud");
 	}
-
+	
 	protected void initComponents(String nameFrame) {
-		setTitle(nameFrame);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 900, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-
+		this.setTitle(nameFrame);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(100, 100, 900, 500);
+		this.contentPane = new JPanel();
+		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.setContentPane(this.contentPane);
+		this.contentPane.setLayout(new BorderLayout(0, 0));
+		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
-		contentPane.add(panel, BorderLayout.WEST);
+		this.contentPane.add(panel, BorderLayout.WEST);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] {30, 110, 30, 0};
+		gbl_panel.columnWidths = new int[] { 30, 110, 30, 0 };
 		gbl_panel.rowHeights = new int[] { 0, 0, 23, 0, 0 };
 		gbl_panel.columnWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		gbl_panel.rowWeights = new double[] { 1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-
-		panel_2 = new JPanel();
-		panel_2.setBackground(Color.DARK_GRAY);
-		panel_2.setLayout(null);
+		
+		this.panel_2 = new JPanel();
+		this.panel_2.setBackground(Color.DARK_GRAY);
+		this.panel_2.setLayout(null);
 		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
 		gbc_panel_2.gridwidth = 3;
 		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
 		gbc_panel_2.fill = GridBagConstraints.BOTH;
 		gbc_panel_2.gridx = 0;
 		gbc_panel_2.gridy = 0;
-		panel.add(panel_2, gbc_panel_2);
-
+		panel.add(this.panel_2, gbc_panel_2);
+		
 		JLabel label_1 = new JLabel();
 		label_1.setIcon(new ImageIcon(GenericFormCadastro.class.getResource("/br/com/estatistica/util/icons/Logo-vers-1(16-09)min.png")));
 		label_1.setForeground(new Color(220, 220, 220));
 		label_1.setFont(new Font("Calibri Light", Font.BOLD, 20));
 		label_1.setBounds(13, 11, 143, 81);
-		panel_2.add(label_1);
-
+		this.panel_2.add(label_1);
+		
 		JPanel panel_4 = new JPanel();
 		panel_4.setBounds(3, 103, 187, 1);
-		panel_2.add(panel_4);
-
+		this.panel_2.add(panel_4);
+		
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(Color.DARK_GRAY);
 		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
@@ -102,5 +100,5 @@ public class GenericFormCadastro extends JFrame {
 		gbc_panel_3.gridx = 0;
 		gbc_panel_3.gridy = 1;
 		panel.add(panel_3, gbc_panel_3);
-    }
+	}
 }
