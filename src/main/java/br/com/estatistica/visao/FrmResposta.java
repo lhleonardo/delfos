b4c9bb2b2472c41354eecffa3de09ea6eb48a14f
Jéssica <Jssica@JSSICA>
2015-09-25 16:01:04 -0300

    Criação da classe FormResposta (Sim, de novo)

---
+++ src/main/java/br/com/estatistica/visao/FrmResposta.java
@@ -0,0 +1,37 @@
+package br.com.estatistica.visao;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class FrmResposta extends GenericFormCadastro {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmResposta frame = new FrmResposta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmResposta() {
+		super();
+	}
+
+}
