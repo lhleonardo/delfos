e8d352d102f1f1dbc42f480dab0f179d81b9af0b
Leonardo Braz <lhleonardo@hotmail.com>
2015-09-25 13:47:23 -0400

    Modificações necessárias

--- src/main/java/br/com/estatistica/Estatistica.java
+++ src/main/java/br/com/estatistica/Estatistica.java
@@ -6,12 +6,9 @@ import javax.swing.UnsupportedLookAndFeelException;
 import br.com.estatistica.visao.FrmLoginUsuario;
 
 public class Estatistica {
-
+	
 	public static void main(String[] args) {
-		
 		try {
-			// define a aplicação com look and feel do sistema.
-			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 			FrmLoginUsuario login = new FrmLoginUsuario();
 			login.setVisible(true);
