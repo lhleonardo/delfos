4ac90c67ef0a8ae0b783e240212bb18be6d7e53f
Leonardo Braz <lhleonardo@hotmail.com>
2015-09-26 23:06:23 -0400

    Cadastro de estados funcionando corretamente.

--- src/main/java/br/com/estatistica/visao/FrmCadastroBairro.java
+++ src/main/java/br/com/estatistica/visao/FrmCadastroBairro.java
@@ -20,7 +20,7 @@ import br.com.estatistica.util.ConnectionFactory;
 import br.com.estatistica.util.Mensagem;
 
 public class FrmCadastroBairro extends GenericFormCadastro {
-	
+
 	private static final long serialVersionUID = 1L;
 	private JPanel panel;
 	private JLabel lblCdigo;
@@ -37,7 +37,7 @@ public class FrmCadastroBairro extends GenericFormCadastro {
 	private JButton btnCancelar;
 	private JScrollPane scrollPane;
 	private BairroDAO bDao;
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
 			try {
@@ -48,106 +48,106 @@ public class FrmCadastroBairro extends GenericFormCadastro {
 			}
 		});
 	}
-	
+
 	public FrmCadastroBairro(Connection connection) {
 		super("Cadastro de Bairros", connection);
 		this.initComponents();
 	}
-	
+
 	private void initComponents() {
 		this.setResizable(false);
-		
+
 		this.panel = new JPanel();
 		this.getContentPane().add(this.panel, BorderLayout.CENTER);
 		this.panel.setLayout(null);
-		
+
 		this.lblCdigo = new JLabel("Código");
 		this.lblCdigo.setBounds(10, 11, 46, 14);
 		this.panel.add(this.lblCdigo);
-		
+
 		this.txtCodigo = new JTextField();
 		this.txtCodigo.setEditable(false);
 		this.txtCodigo.setBounds(10, 25, 46, 20);
 		this.panel.add(this.txtCodigo);
 		this.txtCodigo.setColumns(10);
-		
+
 		this.btnPesquisar = new JButton("");
 		this.btnPesquisar.addActionListener(e -> FrmCadastroBairro.this.btnPesquisarActionPerformed(e));
 		this.btnPesquisar.setBounds(58, 24, 33, 23);
 		this.panel.add(this.btnPesquisar);
-		
+
 		this.lblNome = new JLabel("Nome");
 		this.lblNome.setBounds(101, 11, 46, 14);
 		this.panel.add(this.lblNome);
-		
+
 		this.txtNome = new JTextField();
 		this.txtNome.setBounds(101, 25, 363, 20);
 		this.panel.add(this.txtNome);
 		this.txtNome.setColumns(10);
-		
+
 		this.lblDescrio = new JLabel("Descrição");
 		this.lblDescrio.setBounds(10, 51, 46, 14);
 		this.panel.add(this.lblDescrio);
-		
+
 		this.txtDescricao = new JTextArea();
 		this.scrollPane = new JScrollPane(this.txtDescricao);
 		this.txtDescricao.setLineWrap(true);
 		this.scrollPane.setBounds(10, 65, 454, 219);
 		this.panel.add(this.scrollPane);
-		
+
 		this.panel_1 = new JPanel();
 		this.panel_1.setBounds(10, 295, 454, 43);
 		this.panel.add(this.panel_1);
 		this.panel_1.setLayout(null);
-		
+
 		this.btnNovo = new JButton("Novo");
 		this.btnNovo.addActionListener(arg0 -> FrmCadastroBairro.this.btnNovoActionPerformed(arg0));
 		this.btnNovo.setBounds(0, 0, 92, 43);
 		this.panel_1.add(this.btnNovo);
-		
+
 		this.btnSalvar = new JButton("Salvar");
 		this.btnSalvar.addActionListener(e -> FrmCadastroBairro.this.btnSalvarActionPerformed(e));
 		this.btnSalvar.setBounds(91, 0, 92, 43);
 		this.panel_1.add(this.btnSalvar);
-		
+
 		this.btnExcluir = new JButton("Excluir");
 		this.btnExcluir.addActionListener(e -> FrmCadastroBairro.this.btnExcluirActionPerformed(e));
 		this.btnExcluir.setBounds(183, 0, 92, 43);
 		this.panel_1.add(this.btnExcluir);
-		
+
 		this.btnCancelar = new JButton("Cancelar");
 		this.btnCancelar.setBounds(362, 0, 92, 43);
 		this.panel_1.add(this.btnCancelar);
 	}
-	
+
 	protected void btnNovoActionPerformed(ActionEvent arg0) {
 		this.limpaCampos(this.getContentPane());
 		this.txtNome.requestFocus();
 	}
-	
+
 	protected void btnSalvarActionPerformed(ActionEvent e) {
 		try {
 			Bairro b = new Bairro();
 			b.setId(Integer.parseInt((this.txtCodigo.getText().isEmpty()) ? null : this.txtCodigo.getText()));
 			b.setNome(this.txtNome.getText());
 			b.setDescricao(this.txtDescricao.getText());
-
-			System.out.println(b);
 			
+			System.out.println(b);
+
 			this.bDao = new BairroDAO(this.getConnection());
 			this.bDao.save(b);
 		} catch (SQLException e1) {
 			Mensagem.erro(this, e1);
 		}
-		
+
 	}
-	
+
 	protected void btnExcluirActionPerformed(ActionEvent e) {
 		try {
 			if (!this.txtCodigo.getText().isEmpty()) {
 				Bairro b = new Bairro();
 				b.setId(Integer.parseInt(this.txtCodigo.getText()));
-				
+
 				this.bDao = new BairroDAO(this.getConnection());
 				this.bDao.delete(b);
 			} else {
@@ -157,15 +157,15 @@ public class FrmCadastroBairro extends GenericFormCadastro {
 			Mensagem.erro(this, e1);
 		}
 	}
-	
+
 	protected void btnPesquisarActionPerformed(ActionEvent e) {
 		try {
 			String informacao = "Informe qual o tipo de pesquisa (1-Identificador/2-Nome do Registro)";
-			
-			this.bDao = new BairroDAO(this.getConnection());
-			
-			int valor = Integer.parseInt(Mensagem.entrada(this, informacao));
 
+			this.bDao = new BairroDAO(this.getConnection());
+
+			int valor = Integer.parseInt(Mensagem.entrada(this, informacao));
+			
 			switch (valor) {
 				case 1: {
 					this.abreRegistro(this.pesquisaPorCodigo());
@@ -181,26 +181,25 @@ public class FrmCadastroBairro extends GenericFormCadastro {
 		} catch (SQLException | RuntimeException e1) {
 			Mensagem.erro(this, e1);
 		}
-		
-	}
 
+	}
+	
 	protected Bairro pesquisaPorNome() throws SQLException {
 		List<Bairro> list = this.bDao.get(Mensagem.entrada(this, "Informe o Nome do registro"));
 		if (!list.isEmpty()) {
 			Mensagem.informa(this,
-					"Parece que foram encontrados vários registros... Vamos imprimi-los!\nApós isso, informe o código do registro que deseja.");
+			        "Parece que foram encontrados vários registros... Vamos imprimi-los!\nApós isso, informe o código do registro que deseja.");
 			for (Bairro b : list) {
 				Mensagem.informa(this, b.toString());
 			}
 		}
 		return this.pesquisaPorCodigo();
 	}
-
+	
 	protected Bairro pesquisaPorCodigo() throws SQLException {
-		Bairro bairro;
 		return this.bDao.get(Integer.parseInt(Mensagem.entrada(this, "Informe o código do registro")));
 	}
-
+	
 	private void abreRegistro(Bairro bairro) {
 		if (bairro != null) {
 			if (bairro.getId() != null) {
--- src/main/java/br/com/estatistica/visao/FrmCadastroEstado.java
+++ src/main/java/br/com/estatistica/visao/FrmCadastroEstado.java
@@ -2,9 +2,10 @@ package br.com.estatistica.visao;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -13,42 +14,49 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import br.com.estatistica.dao.EstadoDAO;
+import br.com.estatistica.modelos.Estado;
+import br.com.estatistica.util.ConnectionFactory;
+import br.com.estatistica.util.Mensagem;
+
 public class FrmCadastroEstado extends GenericFormCadastro {
-	
+
 	private static final long serialVersionUID = 1L;
 	private JPanel panel;
 	private JLabel lblCdigo;
-	private JTextField textField;
-	private JButton btnNewButton;
+	private JTextField txtCodigo;
+	private JButton btnPesquisar;
 	private JLabel lblNome;
-	private JTextField textField_1;
+	private JTextField txtNome;
 	private JLabel lblUf;
-	private JTextField textField_2;
+	private JTextField txtUf;
 	private JLabel lblDescrio;
-	private JTextArea textArea;
+	private JTextArea txtDescricao;
 	private JButton btnNovo;
 	private JButton btnSalvar;
 	private JButton btnExcluir;
 	private JButton btnCancelar;
 	private JScrollPane scrollPane_1;
-	
+	private EstadoDAO eDao;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
 			try {
-				FrmCadastroEstado frame = new FrmCadastroEstado(null);
+				FrmCadastroEstado frame = new FrmCadastroEstado(new ConnectionFactory().getConnection());
 				frame.setVisible(true);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		});
 	}
-	
+
 	public FrmCadastroEstado(Connection connection) {
 		super("Cadastro de Estados", connection);
 		this.initComponents();
 	}
-	
+
 	private void initComponents() {
+		this.setSize(567, 386);
 
 		this.panel = new JPanel();
 		this.getContentPane().add(this.panel, BorderLayout.CENTER);
@@ -58,46 +66,46 @@ public class FrmCadastroEstado extends GenericFormCadastro {
 		this.lblCdigo.setBounds(10, 11, 46, 14);
 		this.panel.add(this.lblCdigo);
 
-		this.textField = new JTextField();
-		this.textField.setEditable(false);
-		this.textField.setBounds(10, 25, 46, 20);
-		this.panel.add(this.textField);
-		this.textField.setColumns(10);
+		this.txtCodigo = new JTextField();
+		this.txtCodigo.setEditable(false);
+		this.txtCodigo.setBounds(10, 25, 46, 20);
+		this.panel.add(this.txtCodigo);
+		this.txtCodigo.setColumns(10);
 
-		this.btnNewButton = new JButton("");
-		this.btnNewButton.setBounds(56, 24, 33, 23);
-		this.panel.add(this.btnNewButton);
+		this.btnPesquisar = new JButton("");
+		this.btnPesquisar.addActionListener(e -> FrmCadastroEstado.this.btnPesquisarActionPerformed(e));
+		this.btnPesquisar.setBounds(56, 24, 33, 23);
+		this.panel.add(this.btnPesquisar);
 
 		this.lblNome = new JLabel("Nome");
 		this.lblNome.setBounds(97, 11, 46, 14);
 		this.panel.add(this.lblNome);
 
-		this.textField_1 = new JTextField();
-		this.textField_1.setBounds(99, 25, 218, 20);
-		this.panel.add(this.textField_1);
-		this.textField_1.setColumns(10);
+		this.txtNome = new JTextField();
+		this.txtNome.setBounds(99, 25, 218, 20);
+		this.panel.add(this.txtNome);
+		this.txtNome.setColumns(10);
 
 		this.lblUf = new JLabel("UF");
 		this.lblUf.setBounds(321, 11, 46, 14);
 		this.panel.add(this.lblUf);
 
-		this.textField_2 = new JTextField();
-		this.textField_2.setBounds(321, 25, 46, 20);
-		this.panel.add(this.textField_2);
-		this.textField_2.setColumns(10);
+		this.txtUf = new JTextField();
+		this.txtUf.setBounds(321, 25, 46, 20);
+		this.panel.add(this.txtUf);
+		this.txtUf.setColumns(10);
 
 		this.lblDescrio = new JLabel("Descrição");
 		this.lblDescrio.setBounds(10, 56, 46, 14);
 		this.panel.add(this.lblDescrio);
-		this.scrollPane = new ScrollPane(this.textArea);
 
 		this.scrollPane_1 = new JScrollPane();
 		this.scrollPane_1.setBounds(10, 71, 357, 213);
 		this.panel.add(this.scrollPane_1);
 
-		this.textArea = new JTextArea();
-		this.textArea.setLineWrap(true);
-		this.scrollPane_1.setViewportView(this.textArea);
+		this.txtDescricao = new JTextArea();
+		this.txtDescricao.setLineWrap(true);
+		this.scrollPane_1.setViewportView(this.txtDescricao);
 
 		this.btnNovo = new JButton("Novo");
 		this.btnNovo.addActionListener(e -> FrmCadastroEstado.this.btnNovoActionPerformed(e));
@@ -105,18 +113,126 @@ public class FrmCadastroEstado extends GenericFormCadastro {
 		this.panel.add(this.btnNovo);
 
 		this.btnSalvar = new JButton("Salvar");
+		this.btnSalvar.addActionListener(e -> FrmCadastroEstado.this.btnSalvarActionPerformed(e));
 		this.btnSalvar.setBounds(77, 295, 68, 31);
 		this.panel.add(this.btnSalvar);
 
 		this.btnExcluir = new JButton("Excluir");
+		this.btnExcluir.addActionListener(e -> FrmCadastroEstado.this.btnExcluirActionPerformed(e));
 		this.btnExcluir.setBounds(147, 295, 68, 31);
 		this.panel.add(this.btnExcluir);
 
 		this.btnCancelar = new JButton("Cancelar");
+		this.btnCancelar.addActionListener(e -> FrmCadastroEstado.this.btnCancelarActionPerformed(e));
 		this.btnCancelar.setBounds(284, 295, 83, 31);
 		this.panel.add(this.btnCancelar);
 	}
-	
+
+	protected void btnPesquisarActionPerformed(ActionEvent e) {
+		try {
+			String informacao = "Informe qual o tipo de pesquisa (1-Identificador/2-Nome do Registro)";
+
+			this.eDao = new EstadoDAO(this.getConnection());
+
+			int valor = Integer.parseInt(Mensagem.entrada(this, informacao));
+
+			switch (valor) {
+				case 1: {
+					this.abreRegistro(this.pesquisaPorCodigo());
+					break;
+				}
+				case 2: {
+					this.abreRegistro(this.pesquisaPorNome());
+					break;
+				}
+				default:
+					throw new IllegalArgumentException("Valor inválido.");
+			}
+		} catch (SQLException | RuntimeException e1) {
+			Mensagem.erro(this, e1);
+		}
+
+	}
+
+	private void abreRegistro(Estado estado) {
+		if (estado != null) {
+			if (estado.getId() != null) {
+				this.txtCodigo.setText(String.valueOf(estado.getId()));
+				this.txtNome.setText(estado.getNome());
+				this.txtUf.setText(estado.getUf());
+				this.txtDescricao.setText(estado.getDescricao());
+			} else {
+				throw new NullPointerException("Registro sem identificador.");
+			}
+		} else {
+			throw new NullPointerException("Registro inválido.");
+		}
+	}
+
+	protected Estado pesquisaPorNome() throws SQLException {
+		List<Estado> list = this.eDao.get(Mensagem.entrada(this, "Informe o Nome do registro"));
+		if (!list.isEmpty()) {
+			Mensagem.informa(this,
+					"Parece que foram encontrados vários registros... Vamos imprimi-los!\nApós isso, informe o código do registro que deseja.");
+			for (Estado b : list) {
+				Mensagem.informa(this, b.toString());
+			}
+		}
+		return this.pesquisaPorCodigo();
+	}
+
+	protected Estado pesquisaPorCodigo() throws SQLException {
+		return this.eDao.get(Integer.parseInt(Mensagem.entrada(this, "Informe o código do registro")));
+	}
+
 	protected void btnNovoActionPerformed(ActionEvent e) {
+		super.limpaCampos(this.getContentPane());
+		this.txtCodigo.requestFocus();
+	}
+
+	protected void btnSalvarActionPerformed(ActionEvent e) {
+		this.eDao = new EstadoDAO(this.getConnection());
+		try {
+			Estado estado = new Estado();
+			estado.setId(this.txtCodigo.getText().isEmpty() ? null : Integer.parseInt(this.txtCodigo.getText()));
+			estado.setNome(this.txtNome.getText());
+			estado.setUf(this.txtUf.getText());
+			estado.setDescricao(this.txtDescricao.getText());
+
+			this.eDao.save(estado);
+		} catch (SQLException | RuntimeException e1) {
+			Mensagem.erro(this, e1);
+		}
+	}
+
+	private Estado montaEstado() {
+		Estado estado = new Estado();
+		estado.setId(Integer.parseInt((this.txtCodigo.getText().isEmpty()) ? null : this.txtCodigo.getText()));
+		estado.setNome(this.txtNome.getText());
+		estado.setUf(this.txtUf.getText());
+		estado.setDescricao(this.txtDescricao.getText());
+
+		return estado;
+	}
+
+	protected void btnExcluirActionPerformed(ActionEvent e) {
+		this.eDao = new EstadoDAO(this.getConnection());
+		try {
+			if (!this.txtCodigo.getText().isEmpty()) {
+				Estado b = new Estado();
+				b.setId(Integer.parseInt(this.txtCodigo.getText()));
+
+				this.eDao = new EstadoDAO(this.getConnection());
+				this.eDao.delete(b);
+			} else {
+				Mensagem.erro(this, "É necessário que seja informado o código do registro antes de realizar a exclusão.");
+			}
+		} catch (SQLException e1) {
+			Mensagem.erro(this, e1);
+		}
+	}
+
+	protected void btnCancelarActionPerformed(ActionEvent e) {
+		this.dispose();
 	}
 }
