03d7b293ea57d2c64fc7a5d466cdcd964a343450
Jéssica <Jssica@JSSICA>
2015-09-30 15:57:30 -0300

    FrmResposta

---
+++ src/main/java/br/com/estatistica/visao/FrmRespostas.java
@@ -0,0 +1,45 @@
+package br.com.estatistica.visao;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+
+public class FrmRespostas extends GenericFormCadastro {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmRespostas frame = new FrmRespostas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmRespostas() {
+		super();
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 714, 461);
+		panel.add(scrollPane);
+	}
+}
