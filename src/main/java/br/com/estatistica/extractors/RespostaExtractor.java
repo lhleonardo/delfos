88670b7276f339521469495a7711ab5aec388a11
Jéssica <Jssica@JSSICA>
2015-09-30 21:18:32 -0300

    Classe RespostaExtractor (Criação e Implementação) Criação da classe TableModelResposta

---
+++ src/main/java/br/com/estatistica/extractors/RespostaExtractor.java
@@ -0,0 +1,22 @@
+package br.com.estatistica.extractors;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.estatistica.modelos.Pesquisa;
+import br.com.estatistica.modelos.Resposta;
+
+
+public class RespostaExtractor extends Extractable<Resposta>{
+
+	@Override
+	protected Resposta extractModel(ResultSet rs, Connection con) throws SQLException {
+		Resposta resposta = new Resposta();
+		resposta.setId(rs.getInt("id_resposta"));
+		resposta.setDescricao(rs.getString("descricao"));
+		resposta.setObservacao(rs.getString("observacao"));
+		return resposta;
+
+	}
+}
---
+++ src/main/java/br/com/estatistica/modelos/table/TableModelResposta.java
@@ -0,0 +1,5 @@
+package br.com.estatistica.modelos.table;
+
+public class TableModelResposta {
+
+}
