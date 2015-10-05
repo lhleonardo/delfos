2b5d4ad9c815b1f5ea95d25fc2c5fe68e17b859b
Leonardo Braz <lhleonardo@hotmail.com>
2015-10-05 11:54:30 -0400

    Melhorando o código, retirando warnings

--- src/main/java/br/com/estatistica/extractors/RespostaExtractor.java
+++ src/main/java/br/com/estatistica/extractors/RespostaExtractor.java
@@ -4,12 +4,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import br.com.estatistica.modelos.Pesquisa;
 import br.com.estatistica.modelos.Resposta;
 
-
-public class RespostaExtractor extends Extractable<Resposta>{
-
+public class RespostaExtractor extends Extractable<Resposta> {
+	
 	@Override
 	protected Resposta extractModel(ResultSet rs, Connection con) throws SQLException {
 		Resposta resposta = new Resposta();
@@ -17,6 +15,6 @@ public class RespostaExtractor extends Extractable<Resposta>{
 		resposta.setDescricao(rs.getString("descricao"));
 		resposta.setObservacao(rs.getString("observacao"));
 		return resposta;
-
+		
 	}
 }
