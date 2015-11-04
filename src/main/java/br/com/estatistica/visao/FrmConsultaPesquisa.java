0b269966a4d413def63479ec78c6d69c0e1b9813
MarcoAntonio13 <marcoantonio.tecinf@gmail.com>
2015-11-04 16:52:06 -0400

    Arrumando a Interface

--- src/main/java/br/com/estatistica/visao/FrmConsultaPesquisa.java
+++ src/main/java/br/com/estatistica/visao/FrmConsultaPesquisa.java
@@ -9,6 +9,7 @@ import br.com.estatistica.modelos.Pesquisa;
 import br.com.estatistica.modelos.table.TableModelPesquisa;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import javax.swing.JLabel;
 
 public class FrmConsultaPesquisa extends GenericDialogConsulta<Pesquisa, PesquisaDAO, TableModelPesquisa> {
 
@@ -17,17 +18,29 @@ public class FrmConsultaPesquisa extends GenericDialogConsulta<Pesquisa, Pesquis
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel panel;
+	private JPanel panel_1;
+	private JLabel lblNewLabel;
 
-	public FrmConsultaPesquisa(Frame owner, String title, Connection connection) {
-		super(owner, title, connection);
-		// TODO Auto-generated constructor stub
-		initComponents();
+	public FrmConsultaPesquisa(Frame owner, Connection connection) {
+		super(owner, "Consulta Pesquisa", connection);
+		this.initComponents();
 	}
+	
 	private void initComponents() {
+		setTitle("Consulta de Pesquisa");
 		
 		this.panel = new JPanel();
 		getContentPane().add(this.panel, BorderLayout.CENTER);
 		this.panel.setLayout(null);
+		
+		this.panel_1 = new JPanel();
+		this.panel_1.setBounds(0, 0, 457, 405);
+		this.panel.add(this.panel_1);
+		this.panel_1.setLayout(null);
+		
+		this.lblNewLabel = new JLabel("Código");
+		this.lblNewLabel.setBounds(10, 11, 63, 14);
+		this.panel_1.add(this.lblNewLabel);
 	}
 
 	@Override
@@ -35,6 +48,4 @@ public class FrmConsultaPesquisa extends GenericDialogConsulta<Pesquisa, Pesquis
 		// TODO Auto-generated method stub
 		
 	}
-	
-
 }
