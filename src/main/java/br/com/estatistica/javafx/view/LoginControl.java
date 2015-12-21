24943365fc90728a515a40914ab80db63e8ab7b6
Leonardo Braz <lhleonardo@hotmail.com>
2015-12-21 12:58:10 -0400

    Exemplos de telas com Javafx

---
+++ src/main/java/br/com/estatistica/javafx/view/LoginControl.java
@@ -0,0 +1,15 @@
+package br.com.estatistica.javafx.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * @author Leonardo Braz
+ *
+ */
+public class LoginControl {
+
+	@FXML
+	private Button btnEntrar;
+
+}
